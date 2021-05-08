627ca54e3d5dba8566160046198c8837bd7ed2a1
DerpyCranel <miguel45klm@gmail.com>
2021-05-08 16:14:41 -0500

    first commit

---
+++ js/event.js
@@ -0,0 +1,18 @@
+
+
+
+function alerta(){
+
+
+    Swal.fire({
+        title: 'Hola princesa! que todas tus metas se cumplan y seas muy feliz, recuerda que  cuando tengas malos momentos siempre llegara algo bueno.',
+        showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+      })
+
+
+}
